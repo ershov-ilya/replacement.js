ca794a5db641f0e32e9e7ee150066da1dbbd7a55
ershov-ilya <ershov.ilya@gmail.com>
2015-08-26 14:58:13 +0300

    beta version

---
+++ replacer.js
@@ -0,0 +1,50 @@
+﻿var REPLACER = (function(){
+	var PUBLIC={};
+
+   PUBLIC.parseUTM = function(url){
+        utm_keys=config.utm_keys;
+        if(!url || url == '') url = decodeURI(document.location.search);
+        if(url.indexOf('?') < 0) return {};
+
+        var GET = {},
+            OTHER = [],
+            params = [],
+            key = [],
+            split=[],
+            new_url;
+
+        split = url.split('?');
+        new_url=window.location.pathname;
+        url = split[1];
+
+
+        if(url.indexOf('#')!=-1){
+            url = url.substr(0,url.indexOf('#'));
+        }
+        if(url.indexOf('&') > -1){ params = url.split('&');} else {params[0] = url; }
+
+        var r,z;
+        for (r=0; r<params.length; r++){
+            for (z=0; z<utm_keys.length; z++){
+                if(params[r].indexOf(utm_keys[z]+'=') > -1){
+                    if(params[r].indexOf('=') > -1) {
+                        key = params[r].split('=');
+                        GET[key[0]]=key[1];
+                    }
+                }
+            }
+
+            //
+            key = params[r].split('=');
+            if(utm_keys.indexOf(key[0])==-1) {
+                OTHER.push(params[r]);
+            }
+        }
+        if(OTHER.length) new_url+='?'+OTHER.join('&');
+        if(window.location.hash) new_url+=window.location.hash;
+        if(config.utm_redirect) window.history.pushState(window.history.state, '', new_url);
+        return (GET);
+    };
+
+	return PUBLIC;
+})();
